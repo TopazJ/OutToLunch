770932b16c3f256e1800127945d6487a538316e0
TopazJ <shamez.meghji1@ucalgary.ca>
2020-02-24 15:07:21 -0700

    Re-indented file with 4 spaces.

--- view/src/components/App.jsx
+++ view/src/components/App.jsx
@@ -9,95 +9,95 @@ import Home from './Home.jsx'
 
 class App extends Component {
 
-	constructor(props) {
-		super(props);
-		this.login = this.login.bind(this);
-		this.logout = this.logout.bind(this);
-		this.setLogout = this.setLogout.bind(this);
-		this.initButtons = this.initButtons.bind(this);
-	}
+    constructor(props) {
+        super(props);
+        this.login = this.login.bind(this);
+        this.logout = this.logout.bind(this);
+        this.setLogout = this.setLogout.bind(this);
+        this.initButtons = this.initButtons.bind(this);
+    }
 
 
-	state = {
-		homeLinks:[
-			{id:'about',text:'About'},
-			{id:'contact', text:'Contact'},
+    state = {
+        homeLinks:[
+            {id:'about',text:'About'},
+            {id:'contact', text:'Contact'},
             {id:'nowhere', text:'Nich, please change this navbar.'}
-		],
-		navLinks:[
-			{id:'login', text:'Login', component:LoginForm, props:{login:this.login}},
-			{id:'create-account', text:'Create Account', component:CreateAccount},
-		],
-		user:{
-			status:'anon'
-		}
-	};
-
-	initButtons() {
-		fetch('http://127.0.0.1:8000/auth/status/', {
-            method: 'GET',
-        }).then(res => res.json())
-        .then(data => {
-            if (data.status==='user'){
-                this.login();
-            }
-            else{
-            	this.setLogout();
-			}
-        })
-        .catch(err => console.error("Error:", err));
+        ],
+        navLinks:[
+            {id:'login', text:'Login', component:LoginForm, props:{login:this.login}},
+            {id:'create-account', text:'Create Account', component:CreateAccount},
+        ],
+        user:{
+            status:'anon'
+        }
     };
 
-	login() {
-		this.setState({
-			navLinks: [{id: "logout", text: "Logout", component: Logout, props: {logout: this.logout}}]
-		});
-	}
+    initButtons() {
+        fetch('http://127.0.0.1:8000/auth/status/', {
+            method: 'GET',
+        }).then(res => res.json())
+            .then(data => {
+                if (data.status==='user'){
+                    this.login();
+                }
+                else{
+                    this.setLogout();
+                }
+            })
+            .catch(err => console.error("Error:", err));
+    };
+
+    login() {
+        this.setState({
+            navLinks: [{id: "logout", text: "Logout", component: Logout, props: {logout: this.logout}}]
+        });
+    }
 
 
     logout(){
-		fetch("http://127.0.0.1:8000/auth/logout/", {
+        fetch("http://127.0.0.1:8000/auth/logout/", {
             method:"GET"
         }).then(res => res.json())
             .then(data => {
                 if (data.status === 'success') {
                     this.setLogout();
                 }
-        });
+            });
     }
 
     setLogout(){
-		this.setState({navLinks:[{id:'login', text:'Login', component:LoginForm, props:{login:this.login}},
-			{id:'create-account', text:'Create Account', component:CreateAccount}],user: {status: "anon"}});
-	}
+        this.setState({navLinks:[{id:'login', text:'Login', component:LoginForm, props:{login:this.login}},
+                {id:'create-account', text:'Create Account', component:CreateAccount}],user: {status: "anon"}});
+    }
 
 
-	render() {
-		return (
-			<BrowserRouter>
-			<React.Fragment>
-				<NavBar
-						initButtons={this.initButtons}
-						navLinks={this.state.homeLinks}
-						buttonLinks={this.state.navLinks}/>
-				<Switch>
-					{this.state.navLinks.map(link => (
-						<Route
-							key={link.id}
-							path={"/" + link.id}
-							render={() => (
-								<link.component props={link.props}/>
-							)}
-						/>
-					))}
-					<Route path='*'>
-						<Home/>
-					</Route>
-				</Switch>
-			</React.Fragment>
-			</BrowserRouter>
-		);
-	}
+    render() {
+        return (
+            <BrowserRouter>
+                <React.Fragment>
+                    <NavBar
+                        initButtons={this.initButtons}
+                        navLinks={this.state.homeLinks}
+                        buttonLinks={this.state.navLinks}/>
+                    <Switch>
+                        {this.state.navLinks.map(link => (
+                            <Route
+                                key={link.id}
+                                path={"/" + link.id}
+                                render={() => (
+                                    <link.component props={link.props}/>
+                                )}
+                            />
+                        ))}
+                        <Route path='*'>
+                            <Home/>
+                        </Route>
+                    </Switch>
+                </React.Fragment>
+            </BrowserRouter>
+        );
+    }
 }
 
 export default App;
