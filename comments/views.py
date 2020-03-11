432b9758072f85631a1bee1c3e51bb1bb66d3c54
AM <ArsenicMeatball@yandex.com>
2020-03-10 21:51:09 -0600

    test views added for posts and comments

--- comments/views.py
+++ comments/views.py
@@ -1,7 +1,14 @@
+import requests
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello posts")
+    url = 'https://t5siew2vyb.execute-api.us-east-1.amazonaws.com/alpha/comment'
+    payload = {'commentID': 'null', 'postID': 'testPID1', 'userID': 'null', 'content': 'null', 'dateMS': '0'}
+
+    r = requests.get(url, params=payload)
+    print(r.text)
+
+    return redirect('/')
--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,7 @@
 from django.forms import model_to_dict
 from django.shortcuts import render, redirect
 import json
+import requests
 from django.http import HttpResponse
 
 # Create your views here.
@@ -8,12 +9,20 @@ from posts.validation import validate_create_save, validate_update_save, validat
 
 
 def index(request):
+    url = 'https://c8u8796f4d.execute-api.us-west-2.amazonaws.com/alpha/posts'
+    payload = {'page': '0'}
+
+    r = requests.get(url, params=payload)
+    print(r.text)
+
     return redirect('/')
 
 
 def create_post(request):
     if request.method == 'POST':
+
         data = json.loads(request.body)
+
         return validate_create_save(user_id=data['user_id'],
                                     post_content=data['post_content'],
                                     post_photo_location=data['post_photo_location']
