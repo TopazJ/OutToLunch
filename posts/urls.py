fb43c5713eed397650c258de9f6170b467792ceb
ArsenicMeatball <jeandavid.rousseau@ucalgary.ca>
2020-03-12 22:30:10 -0600

    created views to send events to DynamoDB - untested

--- posts/urls.py
+++ posts/urls.py
@@ -4,7 +4,7 @@ from posts import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('create/', views.create_post, name='create'),
-    path('delete/', views.delete_post, name='delete'),
-    path('update/', views.update_post, name='update'),
+    path('create/', views.create, name='create'),
+    path('delete/', views.delete, name='delete'),
+    path('update/', views.update, name='update'),
 ]
